01f000dc7ccfc78857ba8fab58ce94b83c9bd192
Shalika001 <shalikaupamada@gmail.com>
2023-03-14 01:35:19 +0530

    add button

--- lib/pages/register.dart
+++ lib/pages/register.dart
@@ -3,20 +3,20 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:selling_app/widgets/textFeild.dart';
 
-class Login extends StatefulWidget {
-  const Login({Key? key}) : super(key: key);
+class Register extends StatefulWidget {
+  const Register({Key? key}) : super(key: key);
 
   @override
-  State<Login> createState() => _LoginState();
+  State<Register> createState() => _LoginState();
 }
 
-class _LoginState extends State<Login> {
+class _LoginState extends State<Register> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey[400],
       appBar: AppBar(
-        title: Text('Log in'),
+        title: Text('Register'),
         centerTitle: true,
         backgroundColor: Colors.grey[900],
       ),
@@ -30,62 +30,43 @@ class _LoginState extends State<Login> {
                 backgroundImage: AssetImage(
                     'assets/shalika.jpg'
                 ),
-
                 radius: 40.0,
               ),
             ),
-
             Divider(
               height: 90.0,
               color: Colors.grey[850],
             ),
-
             textField(
-                label: 'Username or Email',
-                labelText: 'Username or Email',
+              label: 'Username or Email',
+              labelText: 'Username or Email',
             ),
-
             SizedBox(height: 50.0),
             textField(
-                label: 'Password',
-                labelText: 'Password',
+              label: 'Password',
+              labelText: 'Password',
             ),
-
-            SizedBox(height: 30.0),
-            Center(
-              child: ElevatedButton(
-                onPressed: () {  },
-                child: Text(
-                    'Login',
-                  style: TextStyle(
-                    color: Colors.black
-                  ),
-                ),
-              ),
-            ),
-
-            SizedBox(height: 30.0),
+            SizedBox(height: 60.0),
             Center(
               child: Text(
-                  'Do not have an account?',
+                'Do not have an account?',
                 style: TextStyle(
                   fontSize: 15.0,
                 ),
               ),
             ),
-
             InkWell(
               child: Column(
                 children: [
                   Center(
-                  child: Text(
-                    'Register',
-                    style: TextStyle(
-                      fontSize: 20.0,
-                      decoration: TextDecoration.underline,
-                      color: Colors.blue[900],
+                    child: Text(
+                      'Register',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        decoration: TextDecoration.underline,
+                        color: Colors.blue,
+                      ),
                     ),
-                  ),
                   ),
                 ],
               ),
