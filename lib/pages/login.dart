6a72af12798a5007cdfe12b18dfd1a63aff5d290
Shalika001 <shalikaupamada@gmail.com>
2023-03-16 18:46:49 +0530

    navigation

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -89,7 +89,7 @@ class _LoginState extends State<Login> {
                     style: TextStyle(
                       fontSize: 20.0,
                       decoration: TextDecoration.underline,
-                      color: Colors.blue[900],
+                      color: Colors.blue,
                     ),
                   ),
                   ),
